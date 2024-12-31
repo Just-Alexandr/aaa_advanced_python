86f57fc544661630088609d94660383c73ecfbf7
Шеслер Александр <shesleraleksandr2004@gmail.com>
2024-12-31 18:57:27 +0300

    5 tasks resolved

--- homework/tasks/task_4.py
+++ homework/tasks/task_4.py
@@ -1,32 +1,29 @@
-async def task_1(i: int):
+async def task_1(i: int, order=''):
+
+    order += '1'
+
     if i == 0:
-        return
+        return order
 
     if i > 5:
-        await task_2(i // 2)
+        return await task_2(i // 2, order)
     else:
-        await task_2(i - 1)
+        return await task_2(i - 1, order)
 
 
-async def task_2(i: int):
+async def task_2(i: int, order=''):
+
+    order += '2'
+
     if i == 0:
-        return
+        return order
 
     if i % 2 == 0:
-        await task_1(i // 2)
+        return await task_1(i // 2, order)
     else:
-        await task_2(i - 1)
+        return await task_2(i - 1, order)
 
 
 async def coroutines_execution_order(i: int = 42) -> int:
-    # Отследите порядок исполнения корутин при i = 42 и верните число, соответствующее ему.
-    #
-    # Когда поток управления входит в task_1 добавьте к результату цифру 1, а когда он входит в task_2,
-    # добавьте цифру 2.
-    #
-    # Пример:
-    # i = 7
-    # return 12212
-    await task_1(i)
-
-    # YOUR CODE GOES HERE
+    order = await task_1(i)
+    return order
